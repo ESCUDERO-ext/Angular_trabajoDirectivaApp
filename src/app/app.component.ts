cf375ce53bbd2b3ee0a0107466c5ca7ac84d7408
ESCUDERO-ext <christian.escudero.external@eviden.com>
2025-04-23 16:54:56 +0200

    Ejercicio Directivas I-II

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo = 'Registro de Usuarios';
+  mensaje="";
+  registrado=false;
+  nombre:string="";
+  apellido:string="";
+  //entradas:object[];
+  entradas: { titulo: string }[]; // Especificamos que es un array de objetos con la propiedad 'titulo'
+
+  constructor(){
+    this.entradas=[
+      {titulo:"Python cada día más presente"},
+      {titulo:"Java presente desde hace más de 20 años"},
+      {titulo:"JavaScript cada vez más funcional"},
+      {titulo:"Kotlin potencia para tus apps"},
+      {titulo:"¿Dónde quedó pascal?"}
+    ];
+  }
+
+  registrarUsuario(){
+
+    this.registrado=true;
+
+    this.mensaje="Usuario registrado con éxito"
+  }
 }
